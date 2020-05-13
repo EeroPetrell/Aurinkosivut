4544c1005953b03ac58da970473dd70a834587cb
EeroPetrell <56298909+EeroPetrell@users.noreply.github.com>
2020-05-13 16:02:55 +0300

    ukk.js siistitty ja kommentoitu

--- UKK.js
+++ UKK.js
@@ -1,15 +1,21 @@
+//luodaan muuttujat html-tiedostosta poimittaville kysymyksille,
+//tiedoston sisäiseen käyttöön luotavien kysymys-olioiden tallentamiseen
+//(mikäli palvelu toteutettaisiin oikeasti, tämä tallennettaisiin johonkin
+//pysyvään tietokantaan) ja indeksin käsittelemiseksi.
 var questions = document.querySelector('#kysymykset');
 var kysymyslista = [];
 var mones = 0;
 
+//funktio jota kutsutaan klikattaessa kysymyssivun "lähetä"-painiketta
 function addQuestion() {
-    //console.log(questions);
-    const form = {}
+
+    const form = {} //yhden kysymyksen tekstin ja tykkäykset tallentava olio
     form.text = document.getElementById("newQuestion").value;
     form.liket = 0;
     form.mones = mones;
-    kysymyslista.push(form);
-    //console.log(kysymyslista);
+    kysymyslista.push(form); //tallennetaan olio listaan
+
+    //luodaan uusi div lisättäväksi html:lään
     const question = document.createElement('div');
     question.classList.add('question');
     question.innerHTML =
@@ -23,15 +29,17 @@ function addQuestion() {
     form.text.value = '';
 }
 
+//funktio jota kutsutaan klikattaessa tykkää-painiketta kysymyskessä
+//(saa parametrikseen tykätyn kysymyksen järjestysnumeron kysymyslistassa)
 function like(i) {
-    //console.log(i);
-    var kymysys = kysymyslista[i];
-    //console.log(kymysys);
-    kymysys.liket += 1;
-    const lapset = questions.childNodes;
-    const moi = lapset[i];
-    console.log(moi);
-    moi.innerHTML = 
+
+    //poimitaan oikea kysymysolio talteen muuttujaan ja lisätään liket-kentän arvoa yhdellä
+    var kysymys = kysymyslista[i];
+    kysymys.liket += 1;
+
+    //poimitaan elementti html-tiedostosta ja vaihdetaan sen html-koodi uuden tykkäysmäärän mukaiseksi
+    const htmlKysymyselementti = questions.childNodes[i];
+    htmlKysymyselementti.innerHTML =
     `   <div class="kysymys">
             <p>${kysymyslista[i].text}</p>
             <button onclick = "like(${kysymyslista[i].mones})" id="likenappi">${kysymyslista[i].liket}<img src="kuvat/like.svg" id="likelogo"></button>
